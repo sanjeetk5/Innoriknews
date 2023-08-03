0e2300afc95bd25e01695ba57442dc3bf2c5201f
sanjeetk5 <112753484+sanjeetk5@users.noreply.github.com>
2023-08-03 20:08:54 +0530

    user authentication routes

---
+++ Backend/index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const { connection } = require("./config/db");
+const { user} = require("./routes/User.routes");
+const app = express();
+require("dotenv").config();
+app.use(express.json());
+
+app.use("/users", user);
+
+app.listen(process.env.port, async () => {
+    try {
+      await connection;
+      console.log("Connected to db");
+    } catch (err) {
+      console.log(err);
+      console.log("Error connecting in database");
+    }
+  
+    console.log(`Server is running at port ${process.env.port}`);
+  });
+  
